dcc00542d68a30fbfc8fd1b70ba485ce5d9abad3
William Ward <william-w@sms.local>
2020-06-03 18:10:53 +0900

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = () => {
+    return (
+        <div className="index">
+            <h1>Welcome to Tokyo Aleworks</h1>
+        </div>
+    );
+}
+
+export default Index;
