eafacf496b09a4e92b12954bfaa577787b6d3cb3
willjw3 <will.j.ward3@gmail.com>
2020-06-08 22:32:02 +0900

    Made header mobile responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "gatsby";
 import Logo from "../images/logo200.png";
-import Slogan from "../images/slogan200.png"
+import MobileLogo from "../images/aleworks90.png"
+import Slogan from "../images/slogan200.png";
+import MobileSlogan from "../images/slogan90.png";
 import "../styles/header.scss";
 
 const Header = () => {
@@ -9,19 +11,24 @@ const Header = () => {
         <div className="header">
             <div className="header-contents">
                 <div className="header-contents-logo">
-                    <img src={Logo} alt=""/>
+                    <img className="header-contents-logo-desktop" src={Logo} alt=""/>
+                    <img className="header-contents-logo-mobile" src={MobileLogo} alt=""/>
                 </div>
                 <div className="header-contents-links">
-                    <ul>
-                        <li>Beers</li>
-                        <li>Brewery</li>
-                        <li>Taprooms</li>
-                    </ul>
+                    <Link className="header-contents-links-link" to="#">Beers</Link>
+                    <Link className="header-contents-links-link" to="#">Brewery</Link>
+                    <Link className="header-contents-links-link" to="#">Taprooms</Link>  
                 </div>
                 <div className="header-contents-slogan">
-                    <img src={Slogan} alt=""/>
+                    <img className="header-contents-slogan-desktop" src={Slogan} alt=""/>
+                    <img className="header-contents-slogan-mobile" src={MobileSlogan} alt=""/>
                 </div>
             </div>
+            <div className="header-mobilelinks">
+                <Link className="header-contents-links-link" to="#">Beers</Link>
+                <Link className="header-contents-links-link" to="#">Brewery</Link>
+                <Link className="header-contents-links-link" to="#">Taprooms</Link>
+            </div>    
         </div>
     );
 }
