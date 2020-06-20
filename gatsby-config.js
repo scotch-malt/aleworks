5673d2a17ba326332b396267677d448684b95783
willjw3 <will.j.ward3@gmail.com>
2020-06-20 18:04:11 +0900

    Using Gatsby Image with Slogan image

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                path: `${__dirname}/src/images`
+                path: `${__dirname}/src/images/`
             }
         },
         `gatsby-plugin-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,24 @@ import Layout from "../components/layout";
 import Footer from "../components/footer";
 import NewsFeed from "../components/newsfeed";
 import Slogan from "../images/TAW_logo.png"
+import Img from "gatsby-image";
 import "../styles/index.scss";
 import "typeface-rubik";
 
 const Index = ({data}) => {
     let news = data.allMarkdownRemark.edges
-    //console.log(news)
+    console.log(data)
     return (
         <Layout>
             <div className="index">
                 <div className="index-main">
-                  <div className="index-main-image">
+                  {/* <div className="index-main-image">
                       <img src={Slogan} alt=""/>
-                  </div>
+                  </div> */}
+                  <Img style={{width: "600px"}}
+                    fluid={data.file.childImageSharp.fluid}
+                    alt="A corgi smiling happily"
+                  />
                   <div className="index-main-card">
                     <h1>THE LATEST NEWS</h1>
                     <NewsFeed news={news} />
@@ -36,6 +41,14 @@ export default Index;
 
 export const pageQuery = graphql`
 query indexQuery {
+  file(relativePath: { eq: "TAW_logo.png" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
   allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
     totalCount
     edges {
