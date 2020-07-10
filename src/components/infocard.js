117ea7f62061db05832c778d168f9cb66ce12e60
William Ward <william-w@sms.local>
2020-07-10 15:13:15 +0900

    Correct display order of infocard elements

--- src/components/infocard.js
+++ src/components/infocard.js
@@ -7,10 +7,10 @@ const InfoCard = ({info}) => {
         <div className="infocard">
             <div className="infocard-top">
                 <h1 className="infocard-top-title">Shop Information</h1>
-                <div className="infocard-top-info" dangerouslySetInnerHTML={{__html: info[0].node.html}} />
+                <div className="infocard-top-info" dangerouslySetInnerHTML={{__html: info[0].node.frontmatter.title === "Itabashi Shop Information" ? info[0].node.html : info[1].node.html}} />
             </div>
             <div className="infocard-bottom">
-                <div dangerouslySetInnerHTML={{__html: info[1].node.html}} />    
+                <div dangerouslySetInnerHTML={{__html: info[0].node.frontmatter.title === "Itabashi Shop Information" ? info[1].node.html : info[0].node.html}} />    
             </div> 
         </div>
     );
