173b32ed0c75179f12f699e6dba492f3089646aa
willjw3 <will.j.ward3@gmail.com>
2020-07-21 13:51:10 +0900

    Added substitute image for news posts without images

--- src/components/newsfeed.js
+++ src/components/newsfeed.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
 import "../styles/newsfeed.scss";
 
 const NewsFeed = ({news})=> {
-
+    const taw = useStaticQuery(graphql`
+      query tawQuery {
+        file(relativePath: { eq: "TAW_logo.png" }) {
+            childImageSharp {
+                # Specify the image processing specifications right in the query.
+                fluid(maxWidth: 600, quality: 100) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+    `)
+    console.log(taw)
     return (
         <div>
             {news.map((item, i) => {
@@ -12,7 +24,7 @@ const NewsFeed = ({news})=> {
                     <div key={i} className="newsfeed">
                         <div className="newsfeed-top">
                             <div className="newsfeed-top-thumb">
-                                <Img fluid={item.node.frontmatter.image.childImageSharp.fluid} alt={item.node.frontmatter.title} style={{border: "1px solid white"}}/>
+                                <Img fluid={item.node.frontmatter.image ? item.node.frontmatter.image.childImageSharp.fluid : taw.file.childImageSharp.fluid} alt={item.node.frontmatter.title} style={{border: "1px solid white", width: "90px"}}/>
                             </div>
                             <div className="newsfeed-top-heading">
                                 <Link className="newsfeed-top-heading-link" to={item.node.fields.slug}><h2>{item.node.frontmatter.title}</h2></Link>
